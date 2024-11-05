e77367feafc00b8c38665e35f3051b91042cf0bd
ummeattiya <ummeattiya7@gmail.com>
2024-11-05 21:41:32 +0500

    first commit

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -16,7 +16,7 @@ const About = () =>{
                         <Image src={profilepicture} alt="profile-picture" width={500} height={400} className="rounded-full " />
 
                     </div>
-                       <div className="mt-9 lg:mt-32 lg:w-[600px] mx-4 lg:mx-0 lg:mx-10">
+                       <div className="mt-9 lg:mt-32 lg:w-[600px] mx-4  lg:mx-10">
                          <p>Hi! I am Umm E Attiya, a passionate web developer currently pursuing my first year in Intermediate in Computer Science (ICS) after completing my matriculation in Biology Science. I have always been fascinated by the digital world and am dedicated to creating innovative and user-friendly solutions</p>
 
                          <p className="mt-4">I am building my skills in technologies such as HTML, CSS, Javascript, and React, which allow me to develop visually appealing and responsive websites. My goal is not just to focus on aesthetics but also enhance the user experience. I am constantly learning new tools and techniques to improve my projects.</p>
--- src/app/Project/page.tsx
+++ src/app/Project/page.tsx
@@ -13,20 +13,20 @@ const Project = () =>{
             </div>
 
               <div className="flex  flex-col  lg:flex-row lg:mt-24">
-                <div className="text-center mt-3 mx-5 md:mx-48 text-center md:mt-3 lg:mt-10 lg:mt-16 lg:mx-5 lg:text-center">
+                <div className="text-center mt-3 mx-5 md:mx-48 md:text-center md:mt-3  lg:mt-16 lg:mx-5 lg:text-center">
                      <Image src={project1} alt="project1" width={500} height={400} />
                      <p className="mt-4 text-2xl font-bold">Project1</p>
                     <a target="_blank" href="https://milestone3-bice-iota.vercel.app/">Click Here </a>
                 </div>
 
-                <div className="text-center mt-3 mx-5 md:mx-48 text-center md: mt-3 lg:ml-4 lg:mt-16 lg:text-center">
+                <div className="text-center mt-3 mx-5 md:mx-48 md:text-center md:mt-3  lg:ml-4 lg:mt-16 lg:text-center">
                     <Image src={project2} alt="project2" width={500} height={400} />
                     <p className="mt-4 text-2xl font-bold">Project2</p>
                     <a target="_blank" href="https://milestone5-nu-black.vercel.app/">Click Here </a>
 
                 </div>
 
-                <div className=" text-center mt-3 mx-5 md:mx-48 text-center md:mt-3  lg:mt-10 lg:ml-4 lg:mt-16 lg:text-center">
+                <div className=" text-center mt-3 mx-5 md:mx-48 md:text-center md:mt-3  lg:ml-4 lg:mt-16 lg:text-center">
                     <Image src={project3} alt="project3" width={500} height={400} />
                     <p className="mt-4 text-2xl font-bold">Project3</p>
                     <a target="_blank" href="https://restaurant-website-six-ochre.vercel.app/">Click Here</a>
